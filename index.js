495d9be35986fb493660050c2abf5b63edcb2fc1
SazuChan <kevin.hoang@viacesi.fr>
2019-10-07 16:21:19 +0200

    ergjlsiqf

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
-
+$( function() {
+    $( "#tabs" ).tabs();
+  } );
+  
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function Opensidenav() 
 {
